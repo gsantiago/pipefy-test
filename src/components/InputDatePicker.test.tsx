800ff1cb6f4d34fa606b834da345b83ab6c11f3e
Guilherme Santiago <guilherme.santiago95@gmail.com>
2020-02-06 13:04:54 -0300

    add more test for input date picker component

--- src/components/InputDatePicker.test.tsx
+++ src/components/InputDatePicker.test.tsx
@@ -7,3 +7,14 @@ it('renders without crashing', () => {
     <InputDatePicker onChange={jest.fn()} />
   )
 })
+
+it('displays the selected date formatted', () => {
+  const { container } = render(
+    <InputDatePicker
+      value='2020-06-10'
+      onChange={jest.fn()}
+    />
+  )
+
+  expect(container.querySelector('input')?.value).toEqual('10/06/2020')
+})
