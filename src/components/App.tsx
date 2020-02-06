2c7297d29a6c66f309d183eb96b356a11d187560
Guilherme Santiago <guilherme.santiago95@gmail.com>
2020-02-05 23:39:04 -0300

    add types

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { useQuery } from '@apollo/react-hooks'
 import { FETCH_FORM_DATA } from '../config/queries'
+import { PublicForm } from '../config/types'
 
 export default function App () {
-  const { loading, error, data } = useQuery(FETCH_FORM_DATA)
+  const { loading, error, data } = useQuery<PublicForm>(FETCH_FORM_DATA)
 
   if (loading) return <div>Loading</div>
 
--- src/config/types.ts
+++ src/config/types.ts
@@ -2,3 +2,32 @@ export interface OptionType {
   label: string
   value: string
 }
+
+// This should be automatically generated from the graphql schema,
+// but I will manually define the interface here
+export interface PublicForm {
+  publicForm: {
+    __typename: 'PublicForm'
+    formFields: FormField[]
+    publicFormSettings: {
+      __typename: 'PublicFormSettings'
+      organizationName: string
+      submitButtonText: string
+      title: string
+    }
+  }
+}
+
+export type FormFieldType = 'ShortTextField' |
+  'LongTextField' |
+  'SelectField' |
+  'RadioVerticalList' |
+  'CheckListVerticalField' |
+  'DateField'
+
+export interface FormField {
+  __typename: FormFieldType
+  id: string
+  label: string
+  options?: string[]
+}
